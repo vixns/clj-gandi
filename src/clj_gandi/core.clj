ff8d23db6b13acda4f49d17e463af3eb29ac83d9
Stéphane Cottin <stephane.cottin@vixns.com>
2014-10-07 08:54:45 +0200

    move fail and retry from the worker to the caller increase rpc timeout add env vars for retry count and log level rename timeout env var

--- src/clj_gandi/core.clj
+++ src/clj_gandi/core.clj
@@ -7,7 +7,8 @@
             [clj-time.format :as format]
             [com.netflix.hystrix.core :as hystrix]
             [clojure.tools.logging :as log]
-            [clojure.core.async :as async :refer (<!! >! <! put! close! chan go-loop)]))
+            [clj-logging-config.log4j :as log-config]
+            [clojure.core.async :as async :refer (<!! >! <! put! close! chan go-loop alts!! timeout)]))
 
 (defn- api-url [] (if (= "1" (env :gandi-prod))
                     "https://rpc.gandi.net/xmlrpc/"
@@ -16,10 +17,12 @@
                       (log/fatal "GANDI_API_KEY env var missing")))
 (def custom-date-formatter (format/formatter "yyyy-MM-dd HH:mm:ss"))
 (def in (chan 1))
+(def latency (Integer/parseInt (or (env :gandi-api-timeout-ms) "30000")))
+(def retry-count (Integer/parseInt (or (env :gandi-api-retry-count) "5")))
 (def slow-chan (throttle-chan in 14 :second))
 
-;;; raise rpc call timeouts to 5000ms, needed when asking hundreds of items
-(System/setProperty "hystrix.command.clj-gandi.core/call*.execution.isolation.thread.timeoutInMilliseconds" "5000")
+(log-config/set-logger! "clj-gandi.core" :level (or (keyword (env :gandi-log-level)) :warn))
+(System/setProperty "hystrix.command.clj-gandi.core/call*.execution.isolation.thread.timeoutInMilliseconds" (str latency))
 
 (hystrix/defcommand call*
                     "gandi api wrapper.
@@ -39,16 +42,11 @@
   (go-loop []
            (let [e (<! slow-chan)]
              (log/debugf "message to gandi worker %s: %s" id e)
-             (loop [retry 0]
-               (let [r (call* e)]
-                 (log/debugf "response from gandi worker %s (try %s): %s " id (inc retry) (if (fault? r) (:fault-string r) r))
-                 (if (or (fault? r) (nil? r))
-                   (if (< retry 4)
-                     (do (Thread/sleep 500) (recur (inc retry)))
-                     (do
-                       (if-let [c (:resp-ch e)] (close! c))
-                       (log/errorf "Error in rpc call %s" (str (:fault-string r) e))))
-                   (if-let [c (:resp-ch e)] (>! c r))))))
+             (let [r (call* e)]
+               (log/debugf "response from gandi worker %s: %s " id (or (:fault-string r) r))
+               (if (fault? r) (log/errorf "Error in rpc call %s" (str (:fault-string r) e)))
+               (if-let [c (:resp-ch e)]
+                 (if-not (nil? r) (>! c r)))))
            (recur)))
 
 (defn initialize
@@ -60,9 +58,19 @@
   "get gandi ressources by method"
   ([method] (call method nil))
   ([method options]
-   (let [r (chan)]
-     (put! in (merge {:method method :resp-ch r} options))
-     (<!! r))))
+   (let [r (chan)
+         req (merge {:method method :resp-ch r} options)]
+     (loop [retry 1]
+       (put! in req)
+       (let [a (alts!! [r (timeout latency)])
+             resp (first a)]
+         (log/debugf "call response on try %s: %s" retry resp)
+         (if (nil? resp)
+           (if (< retry retry-count)
+             (recur (inc retry))
+             (do (close! r) (log/errorf "Error in rpc call %s" (str (:fault-string resp) req))))
+           (do (close! r) (identity resp)))))
+     )))
 
 (defn list-all
   "list all gandi ressources by method."
